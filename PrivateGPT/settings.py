5eccff6cac713fb939bc6b2f9a4d5cbc35fb3d3f
Vikas Sanwal <vikassnwl@gmail.com>
2023-08-06 11:57:31 +0530

    update Artificial Intelligence Development Services.html

--- PrivateGPT/settings.py
+++ PrivateGPT/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+    BASE_DIR / "app/templates"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
