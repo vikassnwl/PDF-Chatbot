8823d5abeb1c0784c453d9a50f207f9b552cd603
Vikas Sanwal <vikassnwl@gmail.com>
2023-08-06 10:21:32 +0530

    add templates

---
+++ app/templates/Artificial Intelligence Development Services_files/jquery-passive-listenor.js
@@ -0,0 +1,1 @@
+jQuery.event.special.touchstart={setup:function(_,ns,handle){this.addEventListener("touchstart",handle,{passive:!ns.includes("noPreventDefault")})}};jQuery.event.special.touchmove={setup:function(_,ns,handle){this.addEventListener("touchmove",handle,{passive:!ns.includes("noPreventDefault")})}}
--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world! You are at the home page.")
+    return render(request, "Artificial Intelligence Development Services.html")
