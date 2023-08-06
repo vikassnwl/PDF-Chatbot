1845bb2370e025a93546d6caf857f5d98f35ca8e
Vikas Sanwal <vikassnwl@gmail.com>
2023-08-07 03:17:12 +0530

    update index.html

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,14 +1,14 @@
 // function to retrieve cookie
-function getCookie(cname){
+function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(";");
-    for(let i=0; i<ca.length; i++){
+    for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == " "){
+        while (c.charAt(0) == " ") {
             c = c.substring(1);
         }
-        if(c.indexOf(name) == 0){
+        if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
@@ -17,17 +17,21 @@ function getCookie(cname){
 
 
 function choose_file() {
+
     // create an input type file element
     var input = document.createElement('input');
     input.type = "file";
 
     // add event listener to the input type file element
     input.addEventListener("change", function (event) {
+
         // get the selected file
         var file = event.target.files[0];
 
-        // do something with the selected file
-        // alert(file.name);
+        // create form data to send it as request
+        const formData = new FormData();
+        formData.append('file', file);
+
         document.getElementById("chosen_filename").innerHTML = file.name;
 
         var filesize = file.size / 1000 / 1000  // filesize in MB
@@ -35,30 +39,61 @@ function choose_file() {
         document.getElementById("chosen_filesize").innerHTML = `${filesize}MB`;
         document.getElementById("chosen_fileinfo").style.visibility = "visible";
 
+        // loading the spinner before calling API
+        spinner = document.getElementById("spinner");
+        spinner.style.display = "flex";
+
         // call fetch api to send file object to the backend
         url = "http://127.0.0.1:8000/upload/"
-        var csrftoken = getCookie('csrftoken');
-        // var headers = new Headers();
-        // headers.append('X-CSRFToken', csrftoken);
-        // headers.append("Content-Type", 'application/json')
-        // headers.append("Accept", 'application/json')
-        // headers.append("Content-Type", 'application/x-www-form-urlencoded')
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-
-        fetch(url, 
-        {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "no-cors",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({name:"Vikas"})
-        })
-        .then(response => console.log(response))
-        // .then(data => console.log(data));
-
+        fetch(url,
+            {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                spinner.style.display = "none";
+            });
     })
 
     // fire the click event
     input.click();
 }
+
+
+
+document.getElementById("query_box")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+
+        query = document.getElementById("query_box").value
+
+        // create form data to send it as request
+        const formData = new FormData();
+        formData.append('query', query);
+
+        // loading the spinner before calling API
+        spinner = document.getElementById("spinner");
+        spinner.style.display = "flex";
+
+        // call api to ask query
+        url = "http://127.0.0.1:8000/ask/"
+        fetch(url,
+            {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                spinner.style.display = "none";
+                if(data["error"] != undefined){
+                    alert(data["error"]);
+                }
+                else{
+                    document.getElementById("response").innerHTML = data["response"];
+                }
+            })
+            .catch(e => alert(e))
+        }
+});
